91d4842041c11f2e6c5a1baf097b281bf207edc3
yxxyun <sean4yang@gmail.com>
2024-01-16 13:50:01 +0800

    Update tvbox.sh

--- .github/scripts/tvbox.sh
+++ .github/scripts/tvbox.sh
@@ -80,5 +80,9 @@ sed -szi 's|"ads":\[.*\]|"ads":\["raw.githubusercontent.com",\
 "z7.cnzz.com",\
 "z8.cnzz.com"\]|' $GITHUB_WORKSPACE/js.json 2>/dev/null
 # rm -rf $GITHUB_WORKSPACE/9918.json
+
+for file in $(find $GITHUB_WORKSPACE/cat/js -maxdepth 1 -type f -name "*.json"); do
+	sed -i s|assets://js|.|g' $file 2>/dev/null
+done
 # sed -szi 's|assets://js|.|g' $GITHUB_WORKSPACE/cat/js/config_open.json $GITHUB_WORKSPACE/cat/js/config_open99.json 2>/dev/null
 
